708f288cc16c2d8ce38537c1d947a6bfee8c63ba
usernamealreadyis <usernamealreadyis@users.noreply.github.com>
2016-06-04 21:08:44 -0700

    *.js

---
+++ js/*.js
@@ -0,0 +1,7 @@
+~ patern_ser
+<-- CNG 00 -->
+RegExp: /\b0x(?:[0-9\b\b0x(?:[0-9A-Fa-f]{6}|0-9A-Fa-f]{8})\b\b0x(?:[0-9A-Fa-f]{6}|0-9A-Fa-f]{8})\b[0-9]+(?:\.[0-9]*)?^A-Fa-f]{6}|0-9A-Fa-f]{8})\b/gxm
+pattern: \b0x(?:[0-9\b\b0x(?:[0-9A-Fa-f]{6}|0-9A-Fa-f]{8})\b\b0x(?:[0-9A-Fa-f]{6}|0-9A-Fa-f]{8})\b[0-9]+(?:\.[0-9]*)?^A-Fa-f]{6}|0-9A-Fa-f]{8})\b
+<a href="#" onclick="close_window();return false;">close</a>
+~ set_url:https://apps.facebook.com/warcommander/?set_%EBp&mixed%3Fcommit=a8b20003a7f0580a28d2bc9cdee99125bc161169%2F%3Fverify_commit%785bbf30b62eba2b82648e4233bc0029175fe2aa/?ref=http://regexr.com/3d9fjf;       
+<-- CNG 00 -->
