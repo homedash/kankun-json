3bf4b84b6990342da0d05e073d069f1b1b0b8852
usernamealreadyis <usernamealreadyis@users.noreply.github.com>
2016-06-21 09:14:38 -0700

    Canvas
    import javafx.scene.*;
    import javafx.scene.paint.*;
    import javafx.scene.canvas.*;
    
    Group root = new Group();
    Scene s = new Scene(root, 300, 300, Color.BLACK);
    
    final Canvas canvas = new Canvas(250,250);
    GraphicsContext gc = canvas.getGraphicsContext2D();
    Canvas is an image that can be drawn on using a set of graphics commands provided by a GraphicsContext.
    
    A Canvas node is constructed with a width and height that specifies the size of the image into which the canvas drawing commands are rendered. All drawing operations are clipped to the bounds of that image.
    
    Example:
    gc.setFill(Color.BLUE);
    gc.fillRect(75,75,100,100);
    
    root.getChildren().add(canvas);

---
+++ js/utils.js
@@ -0,0 +1,41 @@
+var seedrandom = require('seedrandom');
+
+// Create an instance of the prng without a seed (so it'll be a random sequence every time)
+var prng = seedrandom();
+
+var utils = {
+  setRandomSeed: function (seed) {
+    prng = seedrandom(seed);
+  },
+
+  random: function () {
+    return prng();
+  },
+
+  randomInt: function (min, max) {
+    return Math.floor(utils.random() * (max - min + 1)) + min;
+  },
+
+  randomFloat: function (min, max) {
+    return utils.random() * (max - min) + min;
+  },
+
+  randomBoolean: function () {
+    return utils.random() < 0.5;
+  },
+
+  randomDate: function (min, max) {
+    return new Date(Math.floor(utils.random() * (max - min)) + min);
+  },
+
+  randomArrayItem: function (array) {
+    return array[utils.randomInt(0, array.length - 1)];
+  },
+
+  randomChar: function (charset) {
+    charset = charset || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return charset.charAt(utils.randomInt(0, charset.length - 1));
+  }
+};
+
+module.exports = utils;
